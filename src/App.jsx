12f033582874ed66e45234923f3906baab5c639e
HriGrit <ayushsoam15@gmail.com>
2025-04-23 16:11:52 +0300

    deploy: Deployment Set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import { Route, Routes } from "react-router-dom"
-import LandingPage from "./pages/LandingPage"
+
 import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
+import LandingPage from "./pages/LandingPage"
 
 function App() {
+  const deploymentBase = import.meta.env.VITE_DEPLOYMENT_KEY || '';
+  
   return (
-   <div>
-    <Navbar />
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-    </Routes>
-    <Footer /> 
-   </div>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path={`/${deploymentBase}`} element={<LandingPage />} />
+      </Routes>
+      <Footer /> 
+    </div>
   )
 }
 
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/DragNDrop/',
-  plugins: [react(), tailwindcss()],
+  base: '/',
+  plugins: [
+    react(), 
+    tailwindcss()
+  ],
 })
