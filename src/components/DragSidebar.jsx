df4791638517797dbcdef47fcb20e59504767d21
HriGrit <ayushsoam15@gmail.com>
2025-04-26 18:00:05 +0300

    feat: Custom Design

--- src/components/DragSidebar.jsx
+++ src/components/DragSidebar.jsx
@@ -1,27 +1,28 @@
-import { ELEMENT_TYPES } from "../utils/getDefaultProps";
+import { ELEMENT_TYPES, DND_TYPES } from "../utils/getDefaultProps";
 
 function DraggableElement({ type, label }) {
-    const handleDragStart = (e) => {
-      console.log(`Dragging started for type: ${type}`);
-      e.dataTransfer.setData('application/react-flow', type);
-      e.dataTransfer.effectAllowed = 'move';
-    };
-  
-    return (
-      <div
-        draggable
-        onDragStart={handleDragStart}
-        className="p-3 mb-3 border border-gray-300 rounded-lg bg-white shadow-sm cursor-grab hover:shadow-md transition-shadow duration-200 text-center"
-      >
-        {label}
-      </div>
-    );
-  }
+  const handleDragStart = (e) => {
+    console.log(`Dragging NEW element started for type: ${type}`);
+    e.dataTransfer.setData(DND_TYPES.ELEMENT_TYPE, type);
+    e.dataTransfer.effectAllowed = 'move';
+    e.dataTransfer.setData(DND_TYPES.EXISTING_ELEMENT_ID, '');
+  };
+
+  return (
+    <div
+      draggable
+      onDragStart={handleDragStart}
+      className="p-3 mb-3 border text-black font-bold text-lg border-gray-300 rounded-lg bg-[var(--sidebar-text)] shadow-sm cursor-grab hover:shadow-md transition-shadow duration-200 text-center"
+    >
+      {label}
+    </div>
+  );
+}
 
 export default function EditorSidebar() {
     return (
-      <div className="w-full md:w-64 bg-gray-100 p-4 border-r border-gray-200 h-full overflow-y-auto">
-        <h2 className="text-lg font-semibold mb-4 text-gray-700">Elements</h2>
+      <div className="w-full md:w-64 bg-[var(--sidebar-primary)] p-4 border-r min-h-full overflow-y-auto">
+        <h2 className="text-lg font-semibold mb-4 text-[var(--sidebar-text)]">Elements</h2>
         <DraggableElement type={ELEMENT_TYPES.TEXT} label="Text Block" />
         <DraggableElement type={ELEMENT_TYPES.IMAGE} label="Image" />
         <DraggableElement type={ELEMENT_TYPES.BUTTON} label="Button" />
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <footer className="absolute bottom-4 text-sm text-[var(--sidebar-text)]">
+      &copy; {new Date().getFullYear()} Build Your Website Org
+    </footer>
   )
 }
 
